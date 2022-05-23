7dcc8bb583c6e52f8ae1dd0ae100d2f91dfe0613
hojin <redsmile123@naver.com>
2022-05-23 15:03:07 +0900

    test

--- src/main/java/Pack01/MainController.java
+++ src/main/java/Pack01/MainController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-		@RequestMapping("/")
+		@RequestMapping("/asd")
 		String main() {
 			return "index"; 
 		}
